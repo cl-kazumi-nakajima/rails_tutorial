d7e196fa1695739dd2d9e5e96d83f5160ea1a8a0
cl-kazumi-nakajima <kazumi.nakajima@classi.jp>
2021-09-04 14:58:56 +0900

    create microposts page.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_09_04_052038) do
 
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
