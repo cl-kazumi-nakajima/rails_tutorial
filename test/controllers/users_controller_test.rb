f3cac0250b5a65fccf1c2af3d9d34b0d043e53ea
cl-kazumi-nakajima <kazumi.nakajima@classi.jp>
2021-09-12 20:43:15 +0900

    6章

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should get index" do
