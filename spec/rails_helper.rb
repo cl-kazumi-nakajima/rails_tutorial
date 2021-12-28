0e2b4ebd99e9a7482f0b00dc2cc4462a35bbd453
kazumi.nakajima <kazumi.nakajima@classi.jp>
2021-12-28 17:05:00 +0900

    add: System spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,12 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome_headless)
+    # driven_by(:selenium_chrome) # headあり(ふつうの) Chromeをつかうとき
+    # driven_by(:rack_test) # rack_test(高速だがJS実行などブラウザ機能は利用できない)をつかう
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
