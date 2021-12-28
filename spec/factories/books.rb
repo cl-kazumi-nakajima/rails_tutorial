295ae9a43c7d17f80f5c8cdaaa6215470a44fa3d
kazumi.nakajima <kazumi.nakajima@classi.jp>
2021-12-28 18:21:35 +0900

    add: 関連付け定義

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,4 +3,13 @@ FactoryBot.define do
     title { "MyString" }
     author { "MyString" }
   end
+
+  trait :with_variations do
+    after(:create) do |book|
+      book.variations.create!(kind: "paper book")
+      # （動作未確認）上のafterメソッドの代わりに、関連先のFactoryBotがあればこう書ける。
+      # こちらだとFactoryBot.build時に関連先もbuildで作成できるかも。
+      # FactoryBot.create_list(:variation, 2, book: book)
+    end
+  end
 end
--- spec/factories/variations.rb
+++ spec/factories/variations.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :variation do
-    kind { "MyString" }
-    book { nil }
+    kind { "PDF" }
+    book { nil }  # ここに関連を持たせてしまうと、メンテナンスが大変になる(モデルが増えて関連が絡みあってきたときに書きづらくなっていくため)のでやらない。関連付けは、オブジェクト生成時にやる
+    # factory定義に関連を書きたい場合にはtraitをつかうのがお勧め
+    # spec/factories/books.rb 参照
   end
 end
