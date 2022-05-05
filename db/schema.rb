551188b4bbbb9a65f50d021400c24e2cac3d67ff
kazumi.nakajima <kazumi.nakajima@classi.jp>
2022-05-05 20:28:55 +0900

    rails generate migration add_index_to_users_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_070420) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_112833) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_070420) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "variations", force: :cascade do |t|
