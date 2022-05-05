05ec7579b13b25a3bcaa5cefd73ce8bbfce72c65
kazumi.nakajima <kazumi.nakajima@classi.jp>
2022-05-05 16:08:33 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_082623) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_070420) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "variations", force: :cascade do |t|
     t.string "kind"
     t.integer "book_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_variations_on_book_id"
   end
 
