8cd1eab15dbfcb25a44344194e9e8678299df01c
kazumi.nakajima <kazumi.nakajima@classi.jp>
2022-07-22 18:12:52 +0900

    12.2.2 送信メールのテスト

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+  let(:user_michael) { create(:user_michael, reset_token: User.new_token) }
+
   describe "account_activation" do
-    let(:user_michael) { create(:user_michael) }
     let(:mail) { UserMailer.account_activation(user_michael) }
 
     it "renders the headers" do
@@ -17,16 +18,18 @@ RSpec.describe UserMailer, type: :mailer do
   end
 
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:mail) { UserMailer.password_reset(user_michael) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq(["michael@example.com"])
       expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Password reset")
+      expect(mail.body.encoded).to match(user_michael.reset_token)
+      expect(mail.body.encoded).to match(CGI.escape(user_michael.email))
     end
   end
 
