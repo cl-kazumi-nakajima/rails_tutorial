32ca11ada86460fc240bb8e9ae3ad313971836c8
kazumi.nakajima <kazumi.nakajima@classi.jp>
2022-08-25 15:15:35 +0900

    14.1.2 User/Relationshipの関連付け

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,9 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  # 以前のRailsのバージョンでは、このバリデーションが必須だったが、Rails 5から必須ではなくなった
+  # 今回はフォロー機能の実装を優先するが、この手のバリデーションが省略されている可能性があることを頭の片隅で覚えておく
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user_michael) { create(:user_michael) }
+  let!(:user_archer) { create(:user_archer) }
+
+  before do
+    @relationship = Relationship.new(follower_id: user_michael.id,
+                                      followed_id: user_archer.id)
+  end
+
+  it "should be valid" do
+    expect(@relationship).to be_valid
+  end
+
+  it "should require a follower_id" do
+    @relationship.follower_id = nil
+    expect(@relationship).not_to be_valid
+  end
+
+  it "should require a followed_id" do
+    @relationship.followed_id = nil
+    expect(@relationship).not_to be_valid
+  end
 end
