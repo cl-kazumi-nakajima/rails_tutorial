b24eac04f38decf54f55ae24cc39033fff6b350c
kazumi.nakajima <kazumi.nakajima@classi.jp>
2022-08-29 19:14:01 +0900

    14.2.4 ［Follow］ボタン（基本編）

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,15 @@
 class RelationshipsController < ApplicationController
+  before_action :logged_in_user   # これがなくても、current_userがnilになるので例外が起こるが、例外を起こさないためにも入れる
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
 end
--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user_michael) { create(:user_michael) }
+  let!(:user_lana) { create(:user_lana) }
+  let!(:relationship1) { create(:relationship, follower: user_michael, followed: user_lana) }
+
+  it "create should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
+  it "destroy should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationship1)
+    end
+    assert_redirected_to login_url
   end
 end
